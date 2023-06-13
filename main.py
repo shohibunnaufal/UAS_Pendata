3b29429726d56d8552fb8bb2112332131bf10c21
shohibunnaufal <111852743+shohibunnaufal@users.noreply.github.com>
2023-06-13 21:12:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,82 @@ import streamlit as st
 
 tab1, tab2, tab3 = st.tabs(["Home", "Prediction", "Visualisation"])
 
+with tab1:
+    st.title("Aplikasi Prediksi Penyakit Batu Ginjal")
+    
+with tab2:
+    def app(df,x,y):
+        st.title("Halaman Prediksi")
+        col1,col2, col3 = st.columns(3)
+        with col1:
+            bp = st.text_input('Input Nilai bp :')
+            sg = st.text_input('Input Nilai sg :')
+            al = st.text_input('Input Nilai al :')
+            su = st.text_input('Input Nilai su :')
+            rbc = st.text_input('Input Nilai rbc :')
+            pc = st.text_input('Input Nilai pc :')
+            pcc = st.text_input('Input Nilai pcc :')
+            ba = st.text_input('Input Nilai ba :')
+
+        with col2:
+            bgr = st.text_input('Input Nilai bgr :')
+            bu = st.text_input('Input Nilai bu :')
+            sc = st.text_input('Input Nilai sc :')
+            sod = st.text_input('Input Nilai sod :')
+            pot = st.text_input('Input Nilai pot :')
+            hemo = st.text_input('Input Nilai hemo :')
+            pcv = st.text_input('Input Nilai pcv :')
+            wc = st.text_input('Input Nilai wc :')
+
+        with col3:
+            rc = st.text_input('Input Nilai rc :')
+            htn = st.text_input('Input Nilai htn :')
+            dm = st.text_input('Input Nilai dm :')
+            cad = st.text_input('Input Nilai cad :')
+            appet = st.text_input('Input Nilai appet :')
+            pe = st.text_input('Input Nilai pe :')
+            ane = st.text_input('Input Nilai ane :')
+
+        features = [bp, sg, al, su, rbc, pc, pcc, ba, bgr, bu, sc, sod, pot, hemo, pcv, wc, rc, htn, dm, cad, appet, pe, ane]
+
+        #tombol prediksi
+        if st.button("Prediksi"):
+            prediction,score = predict(x,y,features)
+            score = score
+            st.info("Prediksi Sukses...")
+
+            if (prediction ==1):
+                st.warning("Orang tersebut rentan terkena penyakit ginjal")
+            else:
+                st.success("Orang tersebut relatif aman dari penyakit ginjal")
+
+            st.write("Model yang digunakan memiliki tingkat akurasi",(score*100),"%")
+
+with tab3:
+    def app(df,x,y):
+        warnings.filterwarnings('ignore')
+        st.set_option('deprecation.showPyplotGlobaluse', False)
+
+        st.title("Visualisasi Prediksi Batu Ginjal")
+
+        if st.checkbox("Plot Confusion Matrix"):
+            model, score = train_model(x, y)
+            plt.figure(figsize=(10, 6))
+            plot_confusion_matrix(model, x, y, values_format='d')
+            st.pyplot()
+
+        if st.checkbox("Plot Decision Tree"):
+            model, score = train_model(x, y)
+            dot_data = tree.export_graphviz(
+                decision_tree=model,
+                max_depth=3,
+                out_file=None,
+                filled=True,
+                rounded=True,
+                feature_names=x.columns,
+                class_names=['nockd', 'ckd']
+            )
+            st.graphviz_chart(dot_data)
 # membuat sidebar
 st.sidebar.title("Navigasi")
 
